8fe479271b505d24110fb66063e508a9ebc1a019
abdunzelil <93703261+abdunzelil@users.noreply.github.com>
2022-07-13 20:34:02 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get("/", function(req, res){
+  var today = new Date();
+
+  if (today.getDay() === 6 || today.getDay() === 0) {
+    res.write("<h1>It's weekend</h1>");
+  }
+  else {
+    res.sendFile(__dirname + "/index.html");
+  }
+});
+
+
+
+
+
+
+app.listen(3000, function(){
+  console.log("Server started on port 3000");
+});
